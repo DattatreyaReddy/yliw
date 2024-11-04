4a492c0822d0eb6f8fe4994342b514ca9f06e8a4
DattatreyaReddy Panta <58727124+DattatreyaReddy@users.noreply.github.com>
2024-11-04 15:45:38 +0530

    added MPLv2 license

--- lib/src/features/home/presentation/year_home_screen.dart
+++ lib/src/features/home/presentation/year_home_screen.dart
@@ -1,3 +1,9 @@
+// Copyright (c) 2024 Panta Dattatreya Reddy
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
--- lib/src/features/home/service/home_service.dart
+++ lib/src/features/home/service/home_service.dart
@@ -1,3 +1,9 @@
+// Copyright (c) 2024 Panta Dattatreya Reddy
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
